dd6d8a6559831c6e2fc90b55b24f5203cbcaa7a3
Noa Swartz <fetaxyu@gmail.com>
2019-05-07 23:27:06 +0800

    AES encrypt file

--- xender-common/src/main/java/org/warless/xender/common/crypto/AES.java
+++ xender-common/src/main/java/org/warless/xender/common/crypto/AES.java
@@ -3,12 +3,10 @@ package org.warless.xender.common.crypto;
 import org.warless.xender.common.utils.CommonUtils;
 import org.warless.xender.common.utils.CryptoUtils;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
+import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
@@ -56,6 +54,51 @@ public class AES {
     public static String decrypt(String data, String key, String ivKey) {
         byte[] result = decrypt(CryptoUtils.hexStringToBytes(data), key.getBytes(StandardCharsets.UTF_8), ivKey == null ? null : ivKey.getBytes(StandardCharsets.UTF_8));
         return result == null ? null : new String(result, StandardCharsets.UTF_8);
+    }
+
+    private static void write(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int nRead;
+        while ((nRead = in.read(buffer)) != -1) {
+            out.write(buffer, 0, nRead);
+            out.flush();
+        }
+    }
+
+    public static void fileCrypto(String src, String dest, String key, String ivKey, int mode) throws IOException {
+        if (mode != Cipher.DECRYPT_MODE && mode != Cipher.ENCRYPT_MODE) {
+            throw new RuntimeException("Err Mode Code: " + mode);
+        }
+        File srcFile = new File(src);
+        if (srcFile.exists() && srcFile.isFile()) {
+            File destFile = new File(dest);
+            if (!destFile.getParentFile().exists()) {
+                destFile.getParentFile().mkdirs();
+            }
+            destFile.createNewFile();
+        }
+        InputStream in = new FileInputStream(src);
+        OutputStream out = new FileOutputStream(dest);
+        try {
+            SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES");
+            SecretKeySpec secKey = new SecretKeySpec(secretKey.getEncoded(), "AES");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(mode, secKey, new IvParameterSpec(ivKey.getBytes(StandardCharsets.UTF_8)));
+            if (mode == Cipher.DECRYPT_MODE) {
+                CipherOutputStream cout = new CipherOutputStream(out, cipher);
+                write(in, cout);
+                cout.close();
+            }
+            if (mode == Cipher.ENCRYPT_MODE) {
+                CipherInputStream cin = new CipherInputStream(in, cipher);
+                write(cin, out);
+                cin.close();
+            }
+        } catch (NoSuchPaddingException | InvalidAlgorithmParameterException | NoSuchAlgorithmException | InvalidKeyException e) {
+            e.printStackTrace();
+        }
+        in.close();
+        out.close();
     }
 
 }
