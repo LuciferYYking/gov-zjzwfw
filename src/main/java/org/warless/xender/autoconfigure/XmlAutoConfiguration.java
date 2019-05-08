c1f9d8a3b25ee7224e25c40e4fb0601a49735a97
fetaxyu <fetaxyu@gmqil.com>
2019-05-08 10:23:11 +0800

    spring boot autoconfig

---
+++ src/main/java/org/warless/xender/autoconfigure/XmlAutoConfiguration.java
@@ -0,0 +1,14 @@
+package org.warless.xender.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Xender
+ *
+ * @author : yubb
+ * @version : 1.0.0
+ * @date : 2019/5/8
+ */
+@Configuration
+public class XmlAutoConfiguration {
+}
