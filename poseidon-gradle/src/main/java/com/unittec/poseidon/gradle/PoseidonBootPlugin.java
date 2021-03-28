3e57a680c02860f41cb29f03abe52a42200c4bf2
“vbintx <bingdyee@gmail.com>
2021-03-28 21:12:44 +0800

    reset head

--- poseidon-gradle/src/main/java/com/unittec/poseidon/gradle/PoseidonBootPlugin.java
+++ poseidon-gradle/src/main/java/com/unittec/poseidon/gradle/PoseidonBootPlugin.java
@@ -1,7 +1,11 @@
 package com.unittec.poseidon.gradle;
 
+import io.spring.gradle.dependencymanagement.DependencyManagementPlugin;
+import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.springframework.boot.gradle.plugin.SpringBootPlugin;
+import org.springframework.boot.gradle.util.VersionExtractor;
 
 /**
  * @author Bing D. Yee
@@ -9,8 +13,15 @@ import org.gradle.api.Project;
  */
 public class PoseidonBootPlugin implements Plugin<Project> {
 
+    static final String BOM_COORDINATES = "com.unittec.poseidon:poseidon-dependencies:";
+
     @Override
     public void apply(Project project) {
+        project.getPlugins().apply(DependencyManagementPlugin.class);
+        project.getPlugins().apply(SpringBootPlugin.class);
+        String version = VersionExtractor.forClass(PoseidonBootPlugin.class);
+        project.getExtensions().findByType(DependencyManagementExtension.class)
+                .imports((importsHandler) -> importsHandler.mavenBom(PoseidonBootPlugin.BOM_COORDINATES + version));
         System.err.println("---------------------------");
     }
 
